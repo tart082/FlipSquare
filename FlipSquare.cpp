157d8b30bfc216b40bac1ec4be2cb870ef225ad9
Naagattumin <129679239+Naagattumin@users.noreply.github.com>
2024-06-04 03:12:10 +0900

    ゲームの実現
    FlipSquareクラスは概ね完成しました。ランダムな行動を返すAI関数"randomAction"と、プレイヤーの行動を標準入力で受け取る関数"playerAction"関数を実装しました。また、それに伴ってゲームの進行の様子を見れるようにしました。

--- FlipSquare.cpp
+++ FlipSquare.cpp
@@ -1,11 +1,12 @@
 #include "FlipSquare.hpp"
 #include <sstream>
+#include <iostream>
 #include <queue>
 #include <random>
 #include <algorithm>
 #include <vector>
 
-std::mt19937 get_rand_mt(0);
+std::mt19937 mt_for_deck(0);
 
 FlipSquareState::FlipSquareState()
 {
@@ -31,7 +32,7 @@ FlipSquareState::FlipSquareState()
     {
         temp_deck.push_back(i);
     }
-    std::shuffle(temp_deck.begin(), temp_deck.end(), get_rand_mt);
+    std::shuffle(temp_deck.begin(), temp_deck.end(), mt_for_deck);
     for(auto card : temp_deck)
     {
         deck_.push(card);
@@ -45,16 +46,57 @@ FlipSquareState::FlipSquareState()
 
 bool FlipSquareState::isDone() const
 {
-    return NowTurn_ > MaxTurn_;
+    return NowTurn_ > MaxTurn;
 }
 
-void FlipSquareState::advance(const int action)
+void FlipSquareState::advance(const ActionInfo action)
 {
+    int card_ind = action.card_ind_;
+    int center_i = action.i_;
+    int center_j = action.j_;
 
+    int card;
+
+    if(TurnPlayer_ == Player::First)
+    {
+        card = first_cards_[card_ind];
+        auto card_itr = first_cards_.begin() + card_ind;
+        first_cards_.erase(card_itr);
+        distribute_card(Player::First);
+        TurnPlayer_ = Player::Second;
+    }
+    else
+    {
+        card = second_cards_[card_ind];
+        auto card_itr = second_cards_.begin() + card_ind;
+        second_cards_.erase(card_itr);
+        distribute_card(Player::Second);
+        TurnPlayer_ = Player::First;
+    }
+
+    int M = CardSize;
+    int topleft_i = center_i - (M-1)/2;
+    int topleft_j = center_j - (M-1)/2;
+
+    for(int cnt_i = 0; cnt_i < M; cnt_i++)
+    {
+        int i = topleft_i + cnt_i;
+        for(int cnt_j = 0; cnt_j < M; cnt_j++)
+        {
+            int j = topleft_j + cnt_j;
+            int dig = cnt_i*M + cnt_j;
+            if(card & (1 << dig)) {
+                board_[i][j] = 1 - board_[i][j];
+            }
+        }
+    }
+    NowTurn_++;
 }
 
 void FlipSquareState::distribute_card(const Player player)
 {
+    if(deck_.empty()) return;
+
     if(player == Player::First) {
         const int top_deck = deck_.front();
         deck_.pop();
@@ -66,35 +108,63 @@ void FlipSquareState::distribute_card(const Player player)
     }
 }
 
-std::vector<int> FlipSquareState::legalActions() const
+std::vector<ActionInfo> FlipSquareState::legalActions() const
 {
-    std::vector<int> actions;
-    if(TurnPlayer == Player::First)
+    std::vector<ActionInfo> actions;
+    if(TurnPlayer_ == Player::First)
     {
-        for(auto card : first_cards_)
+        for(int card_ind = 0; card_ind < CardNum; card_ind++)
         {
-            actions.push_back(card);
+            for(int i = 1; i <= BoardSize; i++)
+            {
+                for(int j = 1; j <= BoardSize; j++)
+                {
+                    actions.push_back(ActionInfo(card_ind, i, j));
+                }    
+            }
         }
     } else {
-        for(auto card : second_cards_)
+        for(int card_ind = 0; card_ind < CardNum; card_ind++)
         {
-            actions.push_back(card);
+            for(int i = 1; i <= BoardSize; i++)
+            {
+                for(int j = 1; j <= BoardSize; j++)
+                {
+                    actions.push_back(ActionInfo(card_ind, i, j));
+                }    
+            }
         }
     }
     return actions;
 }
 
-// WinningStatus FlipSquareState::getWinningStatus() const
-// {
-//     return first_winning_status_;
-// }
+ScoreInfo FlipSquareState::calcScore() const
+{
+    int first_score = 0;
+    int second_score = 0;
+    for(int i = 1; i <= BoardSize; i++)
+    {
+        for(int j = 1; j <= BoardSize; j++)
+        {
+            if(board_[i][j] == 1) {
+                first_score++;
+            } else {
+                second_score++;
+            }
+        }
+    }
+    return ScoreInfo(first_score, second_score);
+}
 
 std::string FlipSquareState::toString() const
 {
     std::stringstream ss("");
-
-    // ss << "is_first:\t" << this->is_first_ << "\n";
-
+    ss << "---- Now Turn:" << NowTurn_ - 1;
+    if(NowTurn_ != 1)
+    {
+        ss << " (" << ((TurnPlayer_ == Player::First) ? 2 : 1) << "p Turn) -------------------";
+    }
+    ss << '\n';
     const int N = BoardSize;    
     for(int i = 1; i <= N; i++)
     {
@@ -124,7 +194,7 @@ std::string FlipSquareState::toString() const
                     ss << '.';
                 }
             }
-            ss << "  ";
+            ss << "   ";
         }
         ss << '\n';
     }
@@ -143,10 +213,13 @@ std::string FlipSquareState::toString() const
                     ss << '.';
                 }
             }
-            ss << "  ";
+            ss << "   ";
         }
         ss << '\n';
     }
-
+    auto score = calcScore();
+    ss << "First  Score : " << score.first_score_ << '\n';
+    ss << "Second Score : " << score.second_score_ << '\n';
+    ss << '\n';
     return ss.str();
 }
--- FlipSquare.hpp
+++ FlipSquare.hpp
@@ -17,34 +17,48 @@ enum Player
 {
     First,
     Second,
+    PlayerNum,
 };
+
+struct ActionInfo
+{
+    int card_ind_;      // 手札のうちのカードのインデックス
+    int i_;
+    int j_;
+    ActionInfo(const int card_ind, const int i, const int j) : card_ind_(card_ind), i_(i), j_(j) {}
+};
+
+struct ScoreInfo
+{
+    int first_score_;
+    int second_score_;
+    ScoreInfo(const int f, const int s) : first_score_(f), second_score_(s) {}
+};
+
+static const int BoardSize = 8;         // 盤面のサイズ
+static const int CardSize = 3;          // カードのサイズ
+static const int CardNum = 5;           //  各プレイヤーの手札の枚数
+static const int MaxTurn = 10;          // 最大ターン数
 
 class FlipSquareState
 {
 private:
-    static const int BoardSize = 8;        // 盤面のサイズ
-    static const int CardSize = 3;          // カードのサイズ
-    static const int CardNum = 5;           //  各プレイヤーの手札の枚数
-    static const int MaxTurn_ = 20;         // 最大ターン数
-
     // 盤面の色, 先手が黒で1, 後手が白で0
     // 処理を簡単にするために上下左右を1列空ける
     int board_[BoardSize+2][BoardSize+2] = {};
     int NowTurn_ = 1;                       // 現在のターン, 手番ｇ変わるたびに1増える
-    Player TurnPlayer = Player::First;      // 現在のターンプレイヤー
-    // bool is_first_ = true;              // 先手番であるか
+    Player TurnPlayer_ = Player::First;     // 現在のターンプレイヤー
     std::queue<int>  deck_;                 // 山札
     std::vector<int> first_cards_;          // 先手の手札
     std::vector<int> second_cards_;         // 後手の手札
-    // WinningStatus first_winning_status_ = WinningStatus::NONE;
 
 public:
     FlipSquareState();
     bool isDone() const;
-    void advance(const int action);
+    void advance(const ActionInfo action);
     void distribute_card(const Player player);
-    std::vector<int> legalActions() const;
-    // WinningStatus getWinningStatus() const;
+    std::vector<ActionInfo> legalActions() const;
+    ScoreInfo calcScore() const;
     std::string toString() const;
 };
 
